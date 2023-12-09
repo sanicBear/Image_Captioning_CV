19518e129997a0c94862bede12385e6126411259
Sisard <1636087@uab.cat>
2023-12-09 20:20:15 +0100

    split function for separating the data, full code to fine tune T5 model

---
+++ T5ForConditionalGeneration.py
@@ -0,0 +1,140 @@
+import os
+import random
+import torch
+from torch.utils.data import DataLoader
+from transformers import BertTokenizerFast, T5ForConditionalGeneration
+from tqdm import tqdm
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Define paths
+data_dir = "/path/to/flickr8k"
+train_txt = os.path.join(data_dir, "captions.txt")
+val_txt = os.path.join(data_dir, "val_captions.txt")
+train_image_dir = os.path.join(data_dir, "Images")
+val_image_dir = os.path.join(data_dir, "Validation")
+model_path = "/path/to/save/model.pt"
+
+# Hyperparameters
+batch_size = 32
+max_seq_len = 50
+lr = 0.001
+epochs = 10
+
+# Preprocess data
+
+
+def preprocess_data(data_file, image_dir):
+  with open(data_file, "r") as f:
+    lines = f.readlines()
+
+  images, captions = [], []
+  for line in lines:
+    image_name, caption = line.split("\t")
+    images.append(os.path.join(image_dir, image_name))
+    captions.append(caption)
+
+  return images, captions
+
+# Tokenize text
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+
+
+def tokenize(text):
+  return tokenizer(text, padding="max_length", truncation=True, max_length=max_seq_len)
+
+# Image preprocessing
+
+
+def preprocess_image(image_path):
+  image = Image.open(image_path).convert("RGB")
+  image = image.resize((224, 224))
+  image = torch.from_numpy(np.array(image)).float()
+  image = image.permute(2, 0, 1)
+  image = (image / 255.0).unsqueeze(0)
+  return image
+
+# Dataset class
+
+
+class CaptionDataset(Dataset):
+  def __init__(self, images, captions):
+    self.images = images
+    self.captions = captions
+
+  def __len__(self):
+    return len(self.images)
+
+  def __getitem__(self, idx):
+    image_path = self.images[idx]
+    image = preprocess_image(image_path)
+    caption = self.captions[idx]
+    tokenized_caption = tokenize(caption)
+
+    return image, tokenized_caption
+
+# Create datasets and dataloaders
+train_images, train_captions = preprocess_data(train_txt, train_image_dir)
+val_images, val_captions = preprocess_data(val_txt, val_image_dir)
+
+train_dataset = CaptionDataset(train_images, train_captions)
+val_dataset = CaptionDataset(val_images, val_captions)
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+# Load model
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
+
+# Define optimizer and loss function
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+# Train model
+train_losses, val_losses = [], []
+for epoch in range(epochs):
+  train_loss = 0
+  val_loss = 0
+
+  # Train loop
+  model.train()
+  for images, captions in tqdm(train_loader):
+    optimizer.zero_grad()
+
+    # Encode images
+    image_features = model.encoder(input_ids=images)
+
+    # Decode captions
+    decoder_outputs = model.decoder(input_ids=captions["input_ids"], attention_mask=captions["attention_mask"], encoder_hidden_states=image_features)
+
+    # Calculate loss
+    loss = loss_fn(decoder_outputs.logits.view(-1, decoder_outputs.logits.shape[-1]), captions["labels"].view(-1))
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+
+    train_loss += loss.item()
+
+  # Validation loop
+  model.eval()
+  with torch.no_grad():
+    for images, captions in val_loader:
+      # Encode images
+      image_features = model.encoder(input_ids=images)
+
+      # Decode captions
+      decoder_outputs = model.decoder(input_ids=captions["input_ids"], attention_mask=captions["attention_mask"], encoder_hidden_states=image_features)
+
+      # Calculate loss
+      loss = loss_fn(decoder_outputs.logits.view(-1, decoder_outputs.logits.shape[-1]), captions["labels"].view(-1))
+
+      val_loss += loss.item()
+
+  # Save loss
+  train_losses.append(train_loss / len(train_loader))
+  val_losses.append(val_loss / len(val_loader))
+
+  # Save model
+  if epoch == epochs - 1:
+    torch.save(model.state_dict(), os.path.join(model_path, f"model_{epoch}.pth"))
---
+++ split_dataset.py
@@ -0,0 +1,92 @@
+import os
+import random
+import shutil
+from collections import defaultdict
+
+def split_dataset(images_folder, output_folder, train_ratio=0.49, validation_ratio=0.21):
+    # Create output folders if they don't exist
+    for folder in ['train', 'validation', 'test']:
+        folder_path = os.path.join(output_folder, folder)
+        os.makedirs(folder_path, exist_ok=True)
+
+    # Get list of image filenames and captions from the text file
+    with open(os.path.join(images_folder, 'captions.txt'), 'r') as file:
+        lines = file.readlines()
+
+    # Shuffle the data
+    random.shuffle(lines)
+
+    # Create a mapping from image filenames to lists of captions
+    image_caption_mapping = defaultdict(list)
+    for line in lines:
+        parts = line.strip().split(',', 1)
+        image_filename = parts[0]
+        caption = parts[1] if len(parts) > 1 else ""
+        image_caption_mapping[image_filename].append(caption)
+
+    total_images = len(image_caption_mapping)
+    train_size = int(total_images * train_ratio)
+    validation_size = int(total_images * validation_ratio)
+
+    # Split the data
+    train_data = list(image_caption_mapping.items())[:train_size]
+    validation_data = list(image_caption_mapping.items())[train_size:train_size + validation_size]
+    test_data = list(image_caption_mapping.items())[train_size + validation_size:]
+
+    # Write data to txt files for each split
+    splits = [('train', train_data), ('validation', validation_data), ('test', test_data)]
+
+    for split_name, split_data in splits:
+        with open(os.path.join(output_folder, split_name, f'{split_name}_images.txt'), 'w') as file:
+            for image_filename, captions in split_data:
+                for caption in captions:
+                    file.write(f'{image_filename},{caption}\n')
+
+        # Copy images to respective folders
+        split_folder = os.path.join(output_folder, split_name)
+        for image_filename, _ in split_data:
+            source_path = os.path.join(images_folder, 'Images', image_filename)
+            destination_path = os.path.join(split_folder, image_filename)
+
+            # Use shutil.copy instead of os.rename to keep the original images
+            shutil.copy(source_path, destination_path)
+
+
+
+def check_indexes(file1_path, file2_path, file3_path):
+    # Read data from input files
+    data1 = set()
+    with open(file1_path, 'r') as file1:
+        for line in file1:
+            index, caption = line.strip().split(',', 1)
+            data1.add(index)
+
+    data2 = set()
+    with open(file2_path, 'r') as file2:
+        for line in file2:
+            index, caption = line.strip().split(',', 1)
+            data2.add(index)
+
+    data3 = set()
+    with open(file3_path, 'r') as file3:
+        for line in file3:
+            index, caption = line.strip().split(',', 1)
+            data3.add(index)
+
+    # Check for common indexes
+    common_indexes = data1.intersection(data2).union(data2.intersection(data3)).union(data3.intersection(data1))
+
+    # Return True if any common indexes are found, else return False
+    return bool(common_indexes)
+
+if __name__ == "__main__":
+    file1_path = "/home/sisard/Documents/universitat/year_3/semester_1/Computer_Vision/Image_Captioning/git_repo/testing/test/test_images.txt"
+    file2_path = "/home/sisard/Documents/universitat/year_3/semester_1/Computer_Vision/Image_Captioning/git_repo/testing/train/train_images.txt"
+    file3_path = "/home/sisard/Documents/universitat/year_3/semester_1/Computer_Vision/Image_Captioning/git_repo/testing/validation/validation_images.txt"
+
+    has_common_indexes = check_indexes(file1_path, file2_path, file3_path)
+
+    if has_common_indexes:
+        print("Error: Some indexes are repeated between files.")
+    else:
+        print("All indexes are exclusive to their respective files.")
