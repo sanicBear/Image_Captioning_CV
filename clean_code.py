2dafa4158082ece6fe3229f571e52b0f0245b461
Sisard <1636087@uab.cat>
2023-12-20 22:38:59 +0100

    dfdf

--- clean_code.py
+++ clean_code.py
@@ -363,7 +363,7 @@ model = EncoderDecoderModel(
 ).to(device)
 
 # Loss function and Optimizer
-criterion = nn.CrossEntropyLoss(ignore_index=image_dataset.vocabulary.stoi["<PAD>"])
+criterion = nn.CrossEntropyLoss(ignore_index=image_dataset.vocabulary.word_to_index["<PAD>"])
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 
